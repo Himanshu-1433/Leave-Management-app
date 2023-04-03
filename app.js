8e6c27c28b16b801a86f76b6e21dc967085eacd8
Kuldip91093 <kuldip@raoinformationtechnology.com>
2023-04-03 15:45:59 +0530

    https://raoinformationtechnology.atlassian.net/browse/CICO-333

--- app.js
+++ app.js
@@ -3,58 +3,30 @@ let emp_desig = document.getElementById("emp-desig");
 let add_btn = document.getElementById("add-btn");
 let leave_invoice = document.getElementById("leave-invoice");
 let confirmBtn = document.getElementsByClassName("confirmBtn");
+let main_card = document.getElementById("main-card");
 let click = 1;
 $(document).ready(() => {
     $(leave_invoice).hide();
     $(confirmBtn).hide();
 });
 add_btn.addEventListener("click", () => {
-    setPosition(0, 1000);
+    setPosition(0, "120%");
     $(leave_invoice).slideToggle();
     $(confirmBtn).slideToggle();
     $("#user-component").slideToggle();
     $("#user-data").slideToggle();
     if (click == 0) {
-        add_btn.style.transition = "all 1s";
+        add_btn.style.transition = "all 0.4s";
         add_btn.style.transform = "rotate(90deg)";
         emp_name.innerHTML = "Dorothy Boone";
         emp_desig.innerHTML = "PHP Developer";
         add_btn.innerHTML = '<i class="fa-solid fa-plus"></i>';
-        anime({
-            targets: '.leave-invoice',
-            duration: 1000,
-            scale: 0,
-        });
-        anime({
-            targets: '.card',
-            duration: 1000,
-            scale: 0.90,
-        });
-        anime({
-            targets: '.confirmBtn',
-            duration: 1000,
-            scale: 0,
-        });
+        main_card.classList.remove("animate_card");
         click = 1;
-        console.log("0 call");
     }
     else if (click == 1) {
-        anime({
-            targets: '.leave-invoice',
-            duration: 900,
-            scale: 1.010,
-        });
-        anime({
-            targets: '.card',
-            duration: 1000,
-            scale: 1.010,
-        });
-        anime({
-            targets: '.confirmBtn',
-            duration: 900,
-            scale: 1.010,
-        });
-        add_btn.style.transition = "all 1s";
+        main_card.classList.add("animate_card");
+        add_btn.style.transition = "all 0.4s";
         add_btn.style.transform = "rotate(180deg)";
         emp_name.innerHTML = "New Request";
         add_btn.innerHTML = '<i class="fa-solid fa-caret-down"></i>';
@@ -69,66 +41,55 @@ confirm.addEventListener("click", function () {
     $("#user-data").show();
     $(leave_invoice).slideToggle();
     $(confirmBtn).slideToggle();
-    add_btn.style.transition = "all 1s";
+    add_btn.style.transition = "all 0.4s";
     add_btn.style.transform = "rotate(90deg)";
     emp_name.innerHTML = "Dorothy Boone";
     emp_desig.innerHTML = "PHP Developer";
     add_btn.innerHTML = '<i class="fa-solid fa-plus"></i>';
+    main_card.classList.remove("animate_card");
     click = 1;
-    anime({
-        targets: '.leave-invoice',
-        duration: 1000,
-        scale: 0,
-    });
-    anime({
-        targets: '.card',
-        duration: 1000,
-        scale: 0.90,
-    });
-    anime({
-        targets: '.confirmBtn',
-        duration: 1000,
-        scale: 0,
-    });
 })
 let fromLeave = document.getElementById("fromLeave");
 let toLeave = document.getElementById("toLeave");
 function setPosition(pos1, pos2) {
     anime({
         targets: '.confirmBtn',
+        duration: 1200,
         left: pos1,
         delay: 300,
-
     });
     anime({
         targets: '.leave-invoice',
+        duration: 1200,
         left: pos1,
         delay: 100,
 
     });
     anime({
         targets: '.date-time-notice',
+        duration: 1200,
         left: pos1,
         delay: 300,
     });
     anime({
         targets: '.cal',
+        duration: 1200,
         left: pos2,
-        delay: 300,
+        delay: 200,
     });
 }
 let slideRight = () => {
-    setPosition(-1000, 0)
+    setPosition('-120%', 0)
 };
 fromLeave.addEventListener("click", slideRight);
 let btn_save = document.getElementById("btn-save");
 let btn_cancel = document.getElementById("btn-cancel");
 btn_save.addEventListener("click", () => {
-    setPosition(0, 1000);
+    setPosition(0, '120%');
 });
 
 btn_cancel.addEventListener("click", () => {
-    setPosition(0, 1000); 
+    setPosition(0, '120%'); 
 });
 
 function buttonTrackPosition(pos) {
@@ -144,7 +105,7 @@ btn_req.addEventListener("click", () => {
     btn_req.style.color = "black";
     btn_calender.style.color = "white";
     console.log("call");
-    buttonTrackPosition(1)
+    buttonTrackPosition('calc(0% + 3px)')
 });
 
 btn_calender.addEventListener("click", () => {
@@ -152,7 +113,7 @@ btn_calender.addEventListener("click", () => {
     btn_calender.style.color = "black";
     console.log(" 2 ");
     let sc = screen.width;
-    buttonTrackPosition((sc / 2) - 29)
+    buttonTrackPosition('calc(50% - 0px)')
 });
 
 // calendar
